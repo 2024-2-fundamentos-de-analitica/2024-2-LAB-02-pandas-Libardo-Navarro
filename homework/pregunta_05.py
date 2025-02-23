0db5a6458607f45daa700b6cff9380728cf53311
Libardo-Navarro <linavarrop@unal.edu.co>
2025-02-22 20:22:07 -0500

    Laboratorio finalizado

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -5,7 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
-
+import pandas as pd
 def pregunta_05():
     """
     Calcule el valor máximo de `c2` por cada letra en la columna `c1` del
@@ -20,3 +20,5 @@ def pregunta_05():
     E    9
     Name: c2, dtype: int64
     """
+    dataframe = pd.read_csv('files/input/tbl0.tsv', delimiter='\t')
+    return dataframe.groupby('c1')['c2'].max()
